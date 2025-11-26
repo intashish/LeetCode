835343829b8ebb793a5cd0a1aed191a470c544c2
princess-singh <ashishsingh.int@gmail.com>
2025-11-26 20:09:16 +0530

    LINKED LIST - is palinderome

--- src/leetcode/PalindromeLinkedList.java
+++ src/leetcode/PalindromeLinkedList.java
@@ -5,8 +5,8 @@ import java.util.Stack;
 public class PalindromeLinkedList {
     public static void main(String[] args) {
 
-//        ListNode node4 = new ListNode(1,null);
-        ListNode node3 = new ListNode(2,null);
+        ListNode node4 = new ListNode(1,null);
+        ListNode node3 = new ListNode(2,node4);
         ListNode node2 = new ListNode(2,node3);
         ListNode node1 = new ListNode(1,node2);
 
@@ -34,6 +34,39 @@ public class PalindromeLinkedList {
 //
 //        return true;
 
-        
+        //Reverse the second half and compare for palindrome
+        //Using fast and slow
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        ListNode reverseHead = reverseListNode(slow);
+
+        ListNode start = head;
+        ListNode end = reverseHead;
+        while(end != null){ //using end because end will always be smaller.
+            if(start.val != end.val){
+                return false;
+            }
+            start = start.next;
+            end = end.next;
+        }
+        return true;
+    }
+
+    public static ListNode reverseListNode(ListNode node){
+
+        ListNode pre = null;
+        ListNode cur = node;
+
+        while (cur != null){
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return  pre;
     }
 }
